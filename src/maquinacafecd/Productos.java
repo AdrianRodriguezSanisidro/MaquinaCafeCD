5a937198a4cd4c15a0395864a1a209e738a49355
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-03-17 18:30:39 +0100

    Methods and getters added

--- src/maquinacafecd/Productos.java
+++ src/maquinacafecd/Productos.java
@@ -10,5 +10,41 @@ package maquinacafecd;
  * @author Adry
  */
 public class Productos {
+    private String cafe,te,chocolate;
+    int azucar;
+
+    public String getCafe() {
+        return cafe;
+    }
+
+    public String getTe() {
+        return te;
+    }
+
+    public String getChocolate() {
+        return chocolate;
+    }
+
+    public int getAzucar() {
+        return azucar;
+    }
     
+    public void agregarAzucar(int azucar){
+        
+    }
+    public int sumarAzucar(){
+        return azucar;
+    }
+    public int restarAzucar(){
+        return azucar;
+    }
+    public String prepararCafe(){
+        return cafe;
+    }
+    public String prapararTe(){
+        return te;
+    }
+    public String prepararChocolate(){
+        return chocolate;
+    }
 }
