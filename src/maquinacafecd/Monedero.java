a10d3afed9fd456ce896fc4781c237d1f6266655
Adrian Rodriguez Sanisidro <adrysanisidro1@gmail.com>
2017-03-17 19:14:02 +0100

    Coments added

--- src/maquinacafecd/Monedero.java
+++ src/maquinacafecd/Monedero.java
@@ -6,18 +6,32 @@
 package maquinacafecd;
 
 /**
- *
+ *Metodos relacionados con el dinero
  * @author Adry
  */
 public class Monedero {
+    /**
+     * Cantidad de dinero que hay para usar
+     */
     private static float dinero;
-
+/**
+ * Getter de dinero
+ * @return dinero
+ */
     public static float getDinero() {
         return dinero;
     }
+    /**
+     * Introduce dinero
+     * @return dinero
+     */
     public static float meterDinero(){
         return dinero;
     }
+    /**
+     * Devuelve el dinero que hay para usar
+     * @return dinero
+     */
     public static float devolverDinero(){
         return dinero;
     }
